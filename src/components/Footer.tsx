b46045435087e8a470d790385ce71cc8b4ff35f2
suhyeon-dreamtech <msh92@idreamtech.co.kr>
2025-06-04 17:29:42 +0900

    feat: project setting(WIP)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      All rights reserved. job.maengsh@gmail.com
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-black-500 py-5 px-20">
+      <h1 className="font-bold text-2lx text-white">HSKPass</h1>
+    </div>
+  );
+};
+
+export default Header;
