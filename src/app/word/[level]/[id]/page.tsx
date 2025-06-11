8ec590d31faa8789a6f2e52f05d960467f93af36
msuhyeon <job.maengsh@gmail.com>
2025-06-11 13:57:00 +0900

    feat: 병음 재생 api 추가 하여 듣기 기능 추가

--- src/app/word/[level]/[id]/page.tsx
+++ src/app/word/[level]/[id]/page.tsx
@@ -1,7 +1,8 @@
 import { createServerSupabaseClient } from '@/lib/supabase/server';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import HanziWriter from '@/components/HanziWriter';
-import Mark from '@/components/Mark';
+import Bookmark from '@/components/Bookmark';
+import PlayAudioButton from '@/components/PlayAudioButton';
 
 type Props = {
   params: {
@@ -20,25 +21,31 @@ const WordDetail = async ({ params }: Props) => {
   }
 
   const word = data[0].word.split('');
+  const res = await fetch(
+    `https://pinyin-word-api.vercel.app//api/${data[0].word}`
+  );
+  const audioUrl = await res.json();
 
   // TODO: 간체 단어를 번체로 번역해서 한자 하나하나의 뜻 음을 표시 필요
-
   return (
-    <Card className="w-full ">
-      <CardHeader>{data[0].level}급</CardHeader>
-      <CardContent className="text-center">
-        <div className="flex justify-end">
-          <Mark />
-        </div>
-        <div>
-          <h3 className="font-semibold text-2xl ">
-            <HanziWriter characters={word} />
-          </h3>
-          <span className="text-neutral-400 text-lg">[{data[0].pinyin}]</span>
-        </div>
-        <p>{data[0].meaning}</p>
-      </CardContent>
-    </Card>
+    <section>
+      <Card className="w-full">
+        <CardHeader>{data[0].level}급</CardHeader>
+        <CardContent className="text-center">
+          <div className="flex justify-end">
+            <Bookmark />
+            <PlayAudioButton audioUrl={audioUrl.url} />
+          </div>
+          <div>
+            <h3 className="font-semibold text-2xl ">
+              <HanziWriter characters={word} />
+            </h3>
+            <span className="text-neutral-400 text-lg">[{data[0].pinyin}]</span>
+          </div>
+          <p>{data[0].meaning}</p>
+        </CardContent>
+      </Card>
+    </section>
   );
 };
 
