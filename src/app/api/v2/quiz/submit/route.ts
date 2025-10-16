dfd7ecfe72d942c611e42252c5c7f0eae5ce4c55
msuhyeon <job.maengsh@gmail.com>
2025-10-16 18:56:46 +0900

    refactor: 퀴즈 제출 시 문제도 저장 하도록 수정(WIP)

--- src/app/api/v2/quiz/submit/route.ts
+++ src/app/api/v2/quiz/submit/route.ts
@@ -1,24 +1,40 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@/lib/supabase/server';
+import { QuizSubmission } from '@/types/quiz';
 
-type UserAnswer = {
-  question_word_id: string;
-  user_choice_id: string;
-  is_correct: boolean;
-  question_type: string;
-  user_answer: string;
+const handleInsertSession = async (supabase, user, submission) => {
+  const { data: inputedQuiz, error } = await supabase
+    .from('user_quiz_sessions')
+    .insert({
+      user_id: user.id,
+      level: submission.level,
+      duration: submission.duration,
+      score: submission.score,
+      total_questions: submission.questions.length,
+      correct_count: submission.correct_count,
+    })
+    .select('id')
+    .single();
+
+  return { inputedQuiz, error };
 };
 
-type QuizSubmission = {
-  level: string;
-  total_questions: number;
-  correct_answers: number;
-  score: number;
-  duration: number;
-  questions: UserAnswer[];
-  quiz_type: string;
-  correct_count: number;
-  user_answer: string;
+const handleInsertAnswer = async (supabase, insertData) => {
+  const { error: insertAnswerError } = await supabase
+    .from('user_quiz_answers')
+    .insert(insertData)
+    .select('id');
+
+  return insertAnswerError;
+};
+
+const handleInsertQuestion = async (supabase, insertQuestionData) => {
+  const { error } = await supabase
+    .from('quiz_questions')
+    .insert(insertQuestionData)
+    .select('id');
+
+  return error;
 };
 
 export async function POST(request: NextRequest) {
@@ -37,24 +53,15 @@ export async function POST(request: NextRequest) {
       );
     }
 
-    const { data: inputedQuiz, error: insertQuizSessionError } = await supabase
-      .from('user_quiz_sessions')
-      .insert({
-        user_id: user.id,
-        level: submission.level,
-        duration: submission.duration,
-        score: submission.score,
-        total_questions: submission.questions.length,
-        correct_count: submission.correct_count,
-      })
-      .select('id')
-      .single();
+    const { inputedQuiz, error } = await handleInsertSession(
+      supabase,
+      user,
+      submission
+    );
 
-    if (insertQuizSessionError) {
-      console.error(
-        `[ERROR]: INSERT user_quiz_answers ${insertQuizSessionError}`
-      );
-      throw insertQuizSessionError;
+    if (error) {
+      console.error(`[ERROR]: INSERT user_quiz_answers ${error}`);
+      throw error;
     }
 
     const insertData = submission.questions.map((quiz) => ({
@@ -67,14 +74,33 @@ export async function POST(request: NextRequest) {
       user_id: user.id,
     }));
 
-    const { error } = await supabase
-      .from('user_quiz_answers')
-      .insert(insertData)
-      .select('id');
+    const { insertAnswerError } = await handleInsertAnswer(
+      supabase,
+      insertData
+    );
+    const insertQuestionData = submission.questions.map((quiz) => ({
+      word_id: quiz.question_word_id,
+      question_type: quiz.question_type,
+      question_text: quiz.question,
+      options: quiz.options,
+      correct_answer: quiz.correct_answer,
+      tokens: quiz.correct_order,
+      pinyin: quiz.pinyin,
+      meaning: quiz.translation,
+    }));
 
-    if (error) {
-      console.error(`[ERROR]: INSERT user_quiz_answers ${error.message}`);
-      throw error;
+    const { insertQuestionError } = await handleInsertQuestion(
+      supabase,
+      insertQuestionData
+    );
+
+    if (insertQuestionError || insertAnswerError) {
+      console.error('[ERROR] Submit quiz failed:', {
+        insertQuestionError: insertQuestionError?.message || null,
+        insertAnswerError: insertAnswerError?.message || null,
+      });
+
+      throw insertQuestionError || insertAnswerError;
     }
 
     return NextResponse.json({
