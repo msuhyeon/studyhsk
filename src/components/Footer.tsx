26b01b7d7ef57c1bae6d5140809a7ec0d9a1ec2c
msuhyeon <job.maengsh@gmail.com>
2025-10-22 17:51:46 +0900

    style: footer 모바일 레이아웃 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="py-7 row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+    <footer className="py-7 text-sm md:text-md row-start-3 flex gap-[24px] flex-wrap items-center justify-center border-t border-zinc-150">
       All rights reserved. job.maengsh@gmail.com
     </footer>
   );
