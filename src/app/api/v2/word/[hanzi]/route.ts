f35b7b3e5e3fb281b86e8fcc8d82e5fcb2ffb408
msuhyeon <job.maengsh@gmail.com>
2026-01-07 17:32:19 +0900

    fix: build error  - 불필요한 코드 주석처리

--- src/app/api/v2/word/[hanzi]/route.ts
+++ src/app/api/v2/word/[hanzi]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { createClient as createServerClient } from '@/lib/supabase/server';
+// import { createClient as createServerClient } from '@/lib/supabase/server';
 // import { SupabaseClient, User } from '@supabase/supabase-js';
 
 export async function GET(
